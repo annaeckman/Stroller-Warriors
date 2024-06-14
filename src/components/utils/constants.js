b374dff5061b5122f1122ce1308240e8da0a9dea
annaeckman <annaeckman@icloud.com>
2024-06-14 15:48:05 -0700

    add drop down to map section

---
+++ src/components/utils/constants.js
@@ -0,0 +1,12 @@
+export const hotelLeonPlaygrounds = [
+  {
+    name: "Hester St Playground",
+    location: "Bet Grand & Hester Sts",
+    accessible: true,
+    latitude: 40.7171,
+    longitude: -73.9939,
+    "ada-bathroom": true,
+    "sensory-friendly": true,
+    "distance-to-hotel": 0.097,
+  },
+];
