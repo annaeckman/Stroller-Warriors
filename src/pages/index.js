0f0d03590fe76b2ce350092009b4af736dcc09ac
annaeckman <annaeckman@icloud.com>
2024-06-15 12:29:06 -0700

    added event listener for modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,19 @@
 import "./index.css";
 import { hotelLeonPlaygrounds } from "../components/utils/constants";
+
+const playgroundListModal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close");
+const mapSubmitButton = document.querySelector(".map__submit-button");
+// const modalSubmitButton = document.querySelector();
+
+mapSubmitButton.addEventListener("click", openPlaygroundModal);
+
+function openPlaygroundModal() {
+  playgroundListModal.classList.add("modal_opened");
+}
+
+modalCloseButton.addEventListener("click", closePlaygroundModal);
+
+function closePlaygroundModal() {
+  playgroundListModal.classList.remove("modal_opened");
+}
